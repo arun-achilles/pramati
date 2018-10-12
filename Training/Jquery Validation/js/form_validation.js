7ca8737b2c6b433535d9529a6a7b2a844f9eab8f
arun-achilles <arun.abi666@gmail.com>
2018-10-12 17:40:52 +0530

    fully completedwith changes

--- Training/Jquery Validation/js/form_validation.js
+++ Training/Jquery Validation/js/form_validation.js
@@ -1,41 +1,42 @@
 $(document).ready(function() {
+
+	name_regex = /^[A-Za-z]{1}[A-Za-z\s]+$/;
+	email_regex = /^[A-Za-z][A-Za-z0-9._%+-]{0,63}@(?:[A-Za-z0-9-]{1,10}\.){1,125}[A-Za-z]{2,5}$/;
+	mobile_regex = /^([6-9]+[\d]{9}){1}?$/;
+	dob_validator = 0;
 	fields_empty = 0;
 	name_validator = 0;
 	email_validator = 0;
 	mobile_validator = 0;
-	dob_validator = 0;
-	name_regex = /^[A-Za-z]{1}[A-Za-z\s]+$/;
-	email_regex = /^[A-Za-z][A-Za-z0-9._%+-]{0,63}@(?:[A-Za-z0-9-]{1,10}\.){1,125}[A-Za-z]{2,5}$/;
-	mobile_regex = /^([6-9]+[\d]{9}){1}?$/;
 
 	$(".dob").datepicker({
 		format: 'mm-dd-yyyy',
 		minDate: new Date('1900, 1 - 1, 1'),
 		maxDate: '-1d',
-		onClose: function(date){
+		onClose: function(date) {
 			dob_validation(date, $(this));
 		},
 		autoclose: true
 	});
 	//NAME Validation
 	$(".name").on('input', function() {
-		var name_input = $(this).val();
-		name_validator = regex_checker(name_input, name_regex, $(this), "Name");
+		var name = $(this);
+		name_validator = regex_checker(name, name_regex, "Name");
 	});
 	//EMAIL VALIDATION
 	$('.contact_email').on('input', function() {
-		var email_input = $(this).val();
-		email_validator = regex_checker(email_input, email_regex, $(this), "Email ID");
+		var email = $(this);
+		email_validator = regex_checker(email, email_regex, "Email ID");
 	});				
 	//Mobile Validation
 	$('.contact_mobile').on('input',function() {
-		var mobile_input = $(this).val();
-		mobile_validator = regex_checker(mobile_input, mobile_regex, $(this), "Mobile Number");
+		var mobile = $(this);
+		mobile_validator = regex_checker(mobile, mobile_regex, "Mobile Number");
 	});
 						
 });
 	//INSERTION	
-function Insert(){
+function Insert() {
 	var first_name = $("#first_name").val();
 	var last_name = $("#last_name").val();
 	var dob = $("#dob").val();
@@ -50,15 +51,16 @@ function Insert(){
 	if(name_validator==0&&email_validator==0&&mobile_validator==0&&dob_validator==0&&fields_empty==0)
 		build_data(first_name,last_name,email,dob,mobile);
 }
+
 function empty_validation(){
-	$('#user_form input').each(
-    function() {
-      if ($(this).val() < 1) {
-        var error_name = $(this).attr('name');
-        $(this).parent().find('span').text(error_name + "  Cannot be blank");
-      }
+	$('#user_form input').each(function() {
+    if ($(this).val() < 1) {
+      var error_name = $(this).attr('name');
+      $(this).parent().find('span').text(error_name + "  cannot be blank");
+    }
   });
 }
+
 function build_data(first_name,last_name,email,dob,mobile){
 	first_name = capitalizeName($("#first_name").val());
 	last_name = capitalizeName($("#last_name").val());
@@ -66,15 +68,17 @@ function build_data(first_name,last_name,email,dob,mobile){
 		$("#contact tbody").append(row);
 		$("#user_form").trigger("reset");
 }
+
 function dob_validation(date, object){
-	if(new Date(date) < new Date(01/01/1990) || new Date(date) > new Date()){
+	if(new Date(date) < new Date(01/01/1900) || new Date(date) > new Date()){
 		object.parent().find($(".error")).text("Date should be between 1900-present");
 		dob_validator = 1;
-		} else {
-				object.parent().find($(".error")).text("");
-				dob_validator = 0;
-		}
+	} else {
+		object.parent().find($(".error")).text("");
+		dob_validator = 0;
+	}
 }
+
 function capitalizeName(str) {
 	str = str.split(" ");
 	for (var i = 0, x = str.length; i < x; i++) {
@@ -84,8 +88,8 @@ function capitalizeName(str) {
 	return str;
 }
 //regex checker
-function regex_checker(name, regex, object, error_field){
-	if(regex.test(name)){
+function regex_checker(object, regex, error_field){
+	if(regex.test(object.val())){
 		object.parent().find($(".error")).text("");
 		return 0;
 	} 
